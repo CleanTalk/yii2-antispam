6a6088e1321f3bb661add03721fac52419e6934c
Dana Luther <dana.luther@gmail.com>
2022-09-03 14:17:06 -0400

    PHP 8+ Compatibility
    PSR4 namespaces
    Upgraded to current versions: php-antispam, php, phpunit, yii

--- Component.php
+++ Component.php
@@ -1,8 +1,9 @@
 <?php
 namespace cleantalk\antispam;
 
-use Cleantalk;
-use CleantalkRequest;
+use Cleantalk\Cleantalk;
+use Cleantalk\CleantalkRequest;
+use Cleantalk\CleantalkResponse;
 use InvalidArgumentException;
 use Yii;
 use yii\base\Component as BaseComponent;
@@ -134,7 +135,7 @@ class Component extends BaseComponent
 
     /**
      * Create request for CleanTalk API.
-     * @return \CleantalkRequest
+     * @return CleantalkRequest
      */
     protected function createRequest()
     {
@@ -155,9 +156,9 @@ class Component extends BaseComponent
     }
 
     /**
-     * @param \CleantalkRequest $request
+     * @param CleantalkRequest $request
      * @param string $method
-     * @return \CleantalkResponse CleanTalk API call result
+     * @return CleantalkResponse CleanTalk API call result
      * @throws InvalidArgumentException
      */
     protected function sendRequest($request, $method)
